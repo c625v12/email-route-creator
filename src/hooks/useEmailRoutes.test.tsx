69640fae1338a5fac45c4a8b6486323b49ba6add
c625v12 <c625v12@gmail.com>
2024-10-13 11:19:36 -0400

    Initial setup with unit test (#3)
    * Adding initial commit
    
    * adding dependencies
    
    * adding first hook test
    
    * more tests
    
    * rest of the tests for hook tests

---
+++ src/hooks/useEmailRoutes.test.tsx
@@ -0,0 +1,65 @@
+import { describe, expect, vi } from 'vitest';
+import { UseEmailRoutes } from './UseEmailRoutes';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+
+vi.mock('axios');
+
+const mockedAxios = axios as typeof axios;
+
+const mockedReturn = ['email1', 'email2'];
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe('UseEmailRoutes', () => {
+  describe('happy path', () => {
+    it('should fetch zone data', async () => {
+      mockedAxios.get = vi.fn().mockResolvedValue({
+        data: mockedReturn,
+      });
+
+      const queryClient = new QueryClient();
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+
+      const { result } = renderHook(() => UseEmailRoutes(), {
+        wrapper,
+      });
+      await waitFor(() => expect(result.current.data).toEqual(mockedReturn));
+
+      // expect(result).toEqual(mockZoneResult);
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/emails');
+    });
+  });
+  describe('unhappy path', () => {
+    it('should throw an error when API call fails', async () => {
+      const expectedError = '["fetchEmails"] data is undefined';
+      mockedAxios.get = vi.fn().mockResolvedValue(new Error());
+
+      const queryClient = new QueryClient();
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+
+      const { result } = renderHook(() => UseEmailRoutes(), {
+        wrapper,
+      });
+
+      await waitFor(() => {
+        expect(result.current.error?.message).toEqual(expectedError);
+      });
+
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/emails');
+    });
+  });
+});
---
+++ src/hooks/useZone.test.tsx
@@ -0,0 +1,105 @@
+import { describe, expect, vi } from 'vitest';
+import { useZone } from './useZone';
+import { ZoneResult } from 'src/models/zone.dto';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+
+vi.mock('axios');
+
+const mockedAxios = axios as typeof axios;
+
+const mockZoneResult: ZoneResult = {
+  id: '123',
+  name: 'example.com',
+  development_mode: 7200,
+  original_name_servers: ['ns1.example.com', 'ns2.example.com'],
+  original_registrar: 'GoDaddy',
+  original_dnshost: 'Cloudflare',
+  created_on: '2022-01-01T00:00:00.000Z',
+  modified_on: '2022-01-01T00:00:00.000Z',
+  activated_on: '2022-01-01T00:00:00.000Z',
+  owner: {
+    id: '123',
+    type: 'user',
+    name: '',
+  },
+  account: {
+    id: '',
+    name: '',
+  },
+  meta: {
+    cdn_only: false,
+    custom_certificate_quota: 0,
+    dns_only: false,
+    foundation_dns: false,
+    page_rule_quota: 0,
+    phishing_detected: false,
+    step: 0,
+  },
+  vanity_name_servers: [],
+};
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe('fetchZone', () => {
+  describe('useZone - happy path', () => {
+    it('should fetch zone data', async () => {
+      mockedAxios.get = vi.fn().mockResolvedValue({
+        data: mockZoneResult, // Your mocked data structure
+      });
+
+      const queryClient = new QueryClient();
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+
+      const { result } = renderHook(() => useZone(), {
+        wrapper,
+      });
+      await waitFor(() => expect(result.current.data).toEqual(mockZoneResult));
+
+      // expect(result).toEqual(mockZoneResult);
+      expect(axios.get).toHaveBeenCalledWith(
+        'http://localhost:3000/api/cloudflare/zone'
+      );
+    });
+  });
+
+  describe('fetchZone - unhappy path', () => {
+    it('should throw an error when API call fails', async () => {
+      const expectedError = '["fetchZone"] data is undefined';
+      mockedAxios.get = vi.fn().mockResolvedValue(new Error());
+
+      const queryClient = new QueryClient();
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+
+      const { result } = renderHook(() => useZone(), {
+        wrapper,
+      });
+
+      await waitFor(() => {
+        // Check if the error is set
+        expect(result.current.error?.message).toEqual(expectedError);
+      });
+      // Log the entire result to inspect it
+      console.log('Result:', result.current.error);
+
+      // Check if the hook is in an error state and the error is correctly captured
+      // expect(result.current.error).toEqual(mockError);
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        'http://localhost:3000/api/cloudflare/zone'
+      );
+    });
+  });
+});
