29c54b76c8c72956c5f1aea707643dd4baf37b6a
c625v12 <c625v12@gmail.com>
2024-10-14 15:33:47 -0400

    adding final tests

--- frontend/src/pages/home.spec.tsx
+++ frontend/src/pages/home.spec.tsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
 
 import Home from './home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 describe('Home', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Home />);
+    const queryClient = new QueryClient();
+
+    const { baseElement } = render(
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
