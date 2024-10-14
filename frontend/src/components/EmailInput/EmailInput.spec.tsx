1a1167c3ac90fd464aa7f6e18de92103a6e8dcda
c625v12 <c625v12@gmail.com>
2024-10-14 14:58:14 -0400

    test cases

--- frontend/src/components/EmailInput/EmailInput.spec.tsx
+++ frontend/src/components/EmailInput/EmailInput.spec.tsx
@@ -1,10 +1,120 @@
-import { render } from '@testing-library/react';
-
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Mock, vi } from 'vitest';
 import EmailInput from './EmailInput';
+import { useFormContext, FormProvider, useForm } from 'react-hook-form';
+import { useZone } from '../../hooks/useZone';
+import { emailFormatter, handleControlClick } from '../../util/utility';
+import { emailRoutesAtom } from '../../jotai/atoms';
+import { useAtom } from 'jotai';
+import { useEmailRoutes } from '../../hooks/UseEmailRoutes';
 
-describe('EmailInput', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<EmailInput />);
-    expect(baseElement).toBeTruthy();
+// Mock the necessary modules and hooks
+vi.mock('react-hook-form', () => ({
+  useFormContext: vi.fn(),
+  FormProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+  useForm: vi.fn(),
+  register: vi.fn(),
+}));
+
+vi.mock('../../hooks/useZone', () => ({
+  useZone: vi.fn(),
+}));
+
+vi.mock('../../hooks/useEmailRoutes', () => ({
+  useEmailRoutes: vi.fn(),
+}));
+
+vi.mock('jotai');
+
+vi.mock('../../util/utility', () => ({
+  emailFormatter: vi.fn(),
+  handleControlClick: vi.fn(),
+}));
+
+describe('EmailInput Component', () => {
+  const mockSetEmailRoutes = vi.fn();
+  const mockGetValues = vi.fn().mockReturnValue({
+    input: 'test@example.com',
+    selection: 'destination',
+  });
+  const mockSetValue = vi.fn();
+  const mockTrigger = vi.fn();
+  const mockRegister = vi.fn();
+
+  const mockFormMethods = {
+    getValues: mockGetValues,
+    setValue: mockSetValue,
+    trigger: mockTrigger,
+    register: mockRegister,
+    formState: { errors: {} },
+    errors: {},
+  };
+
+  beforeEach(() => {
+    // Mock form context
+    (useFormContext as Mock).mockReturnValue(mockFormMethods);
+
+    // Mock hooks
+    (useZone as Mock).mockReturnValue({
+      data: {
+        name: 'example.com',
+      },
+    });
+
+    (useEmailRoutes as Mock).mockReturnValue({
+      data: ['route1', 'route2'],
+    });
+
+    (useAtom as Mock).mockReturnValue([
+      ['route1', 'route2'],
+      mockSetEmailRoutes,
+    ]);
+
+    // Reset mocks before each test
+    vi.clearAllMocks();
+  });
+
+  it('should render the EmailInput component', () => {
+    // Mock form methods
+    const formMethods = useForm(); // Initialize form methods using react-hook-form
+
+    render(
+      <FormProvider {...formMethods}>
+        <EmailInput />
+      </FormProvider>
+    );
+    const label = screen.getByText('New Email Prefix');
+    expect(label).toBeInTheDocument();
+  });
+
+  it('should handle onBlur event', () => {
+    mockGetValues.mockReturnValueOnce({ input: 'test' });
+    (emailFormatter as Mock).mockReturnValue('formatted@example.com');
+    const formMethods = useForm(); // Initialize form methods using react-hook-form
+
+    render(
+      <FormProvider {...formMethods}>
+        <EmailInput />
+      </FormProvider>
+    );
+
+    const input = screen.getByRole('button');
+    fireEvent.blur(input);
+
+    expect(mockTrigger).toHaveBeenCalledWith('input');
+    expect(mockSetValue).toHaveBeenCalledWith('input', 'formatted@example.com');
+  });
+
+  it('should handle onClick event', () => {
+    mockGetValues.mockReturnValueOnce({ input: 'test' });
+    (handleControlClick as Mock).mockReturnValue('clicked@example.com');
+
+    render(<EmailInput />);
+    const input = screen.getByRole('button');
+    fireEvent.click(input);
+
+    expect(mockSetValue).toHaveBeenCalledWith('input', 'clicked@example.com');
   });
 });
--- frontend/src/components/select/select.spec.tsx
+++ frontend/src/components/select/select.spec.tsx
@@ -4,7 +4,6 @@ import '@testing-library/jest-dom';
 import { Mock, vi } from 'vitest';
 import { useFormContext } from 'react-hook-form';
 import Select from './select';
-import { before } from 'node:test';
 
 vi.mock('react-hook-form', () => ({
   useFormContext: vi.fn(),
@@ -39,5 +38,28 @@ describe('Select Component', () => {
     fireEvent.click(screen.getByText('Option 1'));
 
     expect(mockSetValue).toHaveBeenCalledWith('selection', 'Option 1');
+  });
+
+  it('should display error message when errors.input is present', () => {
+    const options = ['Option 1', 'Option 2'];
+
+    const mockErrors = {
+      selection: {
+        message: 'This is a test error message',
+      },
+    };
+
+    (useFormContext as Mock).mockReturnValue({
+      register: vi.fn(),
+      setValue: mockSetValue,
+      watch: mockWatch,
+      formState: { errors: mockErrors },
+    });
+
+    render(<Select label="Test Label" options={options} />);
+
+    const errorMessage = screen.getByText('This is a test error message');
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toHaveClass('p-error');
   });
 });
